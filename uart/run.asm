4d67d0b2345c2a76de18a66b18c19da1c7656f0d
j <j@h>
2017-03-13 21:11:12 +0100

    Added code to read the program flash of the AVR and dump it to the serial outpt.

--- uart/run.asm
+++ uart/run.asm
@@ -14,6 +14,9 @@
 .def delay_reg2 = r19
 .def delay_reg3 = r20
 .def reg_bitmask = r21
+.def reg_tmpstorage = r22
+.def reg_tmp1 = r23
+.def reg_tmp2 = r24
 
 ; At origin 0x0000, hook in our calls.. Why?
 .ORG 0x0000
@@ -26,17 +29,74 @@ main:
     ldi reg_bitmask, (1<<DDB3) | (1<<DDB5)
     out DDRB, reg_bitmask
     cbi PORTB, 3
-    cbi PORTB, 5
+    sbi PORTB, 5
 
-program_loop:
-    ldi reg2, 65 ; load send-data with A
-    sbi PORTB, 3
-    call delay_start ; wait 1 second
+wait_for_trigger:
+	sbis PINB, 4
+	rjmp wait_for_trigger
+	cbi PORTB, 5
+
+	; Dump the entire bootloader into the serial stream
+	; From the inc file, the bootloader starts at:
+	; NRWW_START_ADDR
+	; and ends at 
+	; NRWW_STOP_ADDR
+	;ldi ZH, HIGH(RWW_START_ADDR)
+	;ldi ZL, LOW(RWW_START_ADDR)
+	ldi ZH, 0
+	ldi ZL, 0
+	; Send a preamble so i know where the data starts from
+	; preamble is
+	; ===!{
+	ldi reg2, 0x3d
     call usart_transmit ; send data
-    cbi PORTB, 3
-    cbi PORTB, 5
-    call delay_start ; wait 1 second
-    jmp program_loop
+    call usart_transmit ; send data
+    call usart_transmit ; send data
+	ldi reg2, 0x21
+    call usart_transmit ; send data
+	ldi reg2, 0x7B
+    call usart_transmit ; send data
+program_loop:
+	lpm reg_tmpstorage, Z+
+    ;sbi PORTB, 3
+    mov reg2, reg_tmpstorage ; load send-data with first byte of bootloader
+    call usart_transmit ; send data
+	;mov reg2, ZH
+	;call usart_transmit
+	;mov reg2, ZL
+	;call usart_transmit
+    ;cbi PORTB, 3
+    ;call delay_start ; wait 1 second
+
+	ldi reg_tmp1, HIGH(NRWW_STOP_ADDR)
+	cp reg_tmp1, ZH
+	breq check_address_lsb
+	jmp program_loop
+
+; check last part of address if complete
+check_address_lsb:
+    ;sbi PORTB, 3
+    ;call delay_start ; wait 1 second
+	ldi reg_tmp1, LOW(NRWW_STOP_ADDR)
+	cp reg_tmp1, ZL
+	brne program_loop ; not equal, go back to reading data
+	; if we reach this, then terminate the memory dump
+
+	; send a finishing "postamble"
+	; }!===
+	ldi reg2, 0x7D	
+    call usart_transmit 
+	ldi reg2, 0x21
+    call usart_transmit 
+	ldi reg2, 0x3d
+    call usart_transmit 
+    call usart_transmit 
+    call usart_transmit 
+
+	sbi PORTB, 3
+null_loop:
+	rjmp null_loop
+
 
     ; Prepares the AVR for UART communication
     ; by setting correct bits in the appropriate registers
@@ -73,7 +133,6 @@ usart_transmit:
     sbrs reg1, UDRE0
     rjmp usart_transmit
     ; Put data into buffer, which will send it
-    sbi PORTB, 5
     sts UDR0, reg2
     ret
 
